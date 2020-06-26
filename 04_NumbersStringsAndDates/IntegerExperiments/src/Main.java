4bb0db91ff3a562e5ecdbf7b7b2ce20e88197781
Vika Goriacheva <grumbla@yandex.ru>
2020-06-26 15:42:15 +0300

    Integer, String, Character

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,33 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
         Container container = new Container();
         container.count += 7843;
 
-    }
+        System.out.println(sumDigit(12345));
+        System.out.println(sumDigits(12345));
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
+        System.out.println(sumDigit(5059191));
+        System.out.println(sumDigits(5059191));
+
+        System.out.println(sumDigit(10));
+        System.out.println(sumDigits(10));
+    }
+    public static int sumDigit(int i) {
+        return String.valueOf(i).chars().map(Character::getNumericValue).sum();
+    }
+    public static Integer sumDigits(int number) {
+        String s = Integer.toString(number);
+        int dlina = s.length()-1;
+
+        Integer vsaSumma = 0;
+        for (int poziciya = 0; poziciya <= dlina; poziciya++) {
+
+            int chislo = Integer.parseInt(String.valueOf(s.charAt(poziciya)));
+            vsaSumma = vsaSumma + chislo;
+        }
+        return vsaSumma;
     }
 }
