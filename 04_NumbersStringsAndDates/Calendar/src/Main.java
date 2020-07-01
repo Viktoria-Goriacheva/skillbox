930f406a7dc805dbf9249c281c71ea6fa5b1919e
Vika Goriacheva <grumbla@yandex.ru>
2020-07-01 17:24:37 +0300

    Date.Variant 2

--- 04_NumbersStringsAndDates/Calendar/src/Main.java
+++ 04_NumbersStringsAndDates/Calendar/src/Main.java
@@ -1,6 +1,7 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class Main {
@@ -10,12 +11,29 @@ public class Main {
     int month = 03;
     int myYear = 1989;
     int year = Calendar.getInstance().get(Calendar.YEAR);
-    int old = year-myYear;
+    int old = year - myYear;
     Calendar c = Calendar.getInstance();
-    for (int i=0; i <= old; i++){
-    c.set(myYear, month-1, day);
-    myYear++;
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy '-' E", Locale.ENGLISH );
-    System.out.println(i + " - " + dateFormat.format(c.getTime()));}
+    Date currentDate = new Date();
+    System.out.println("Date 2: " + currentDate);
+    c.set(Calendar.MONTH, month - 1);
+    c.set(Calendar.DATE, day);
+    Date dateOne = c.getTime();
+    System.out.println("Date 1: " + dateOne);
+
+    if (currentDate.after(dateOne)) {
+      for (int i = 0; i <= old; i++) {
+        c.set(myYear, month - 1, day);
+        myYear++;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy '-' E", Locale.ENGLISH);
+        System.out.println(i + " - " + dateFormat.format(c.getTime()));
+      }
+    } else {
+      for (int i = 0; i <= (old - 1); i++) {
+        c.set(myYear, month - 1, day);
+        myYear++;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy '-' E", Locale.ENGLISH);
+        System.out.println(i + " - " + dateFormat.format(c.getTime()));
+      }
+    }
   }
 }
